04bfc87dacd3648d5ea3e1ac6a4a99731e3b2db7
José Gallegos <jose.gallegos.delcarpio@gmail.com>
2022-10-24 12:44:34 -0500

    carpetas en controladores

---
+++ app/Http/Controllers/Catalogues/CountryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Catalogues;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    //
+}
