c872a53e2f194ea9a56dd770fcacb14c36e7ef0c
Carlos Toledo <73927158+Carlosafk@users.noreply.github.com>
2022-10-24 17:30:16 -0500

    correcion a seeder

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -16,38 +16,38 @@ class CitySeeder extends Seeder
     public function run()
     {
         //states->Nuevo León
-       $city =  City::create(["name" => "Abasolo", "states_id" =>1]);
-       $city2 = City::create(["name" => "Bustamante", "states_id" =>1]);
-       $city3 = City::create(["name" => "Cadereyta Jiménez", "states_id" =>1]);
-       $city4 = City::create(["name" => "Doctor González", "states_id" =>1]);
-       $city5 = City::create(["name" => "San Pedro Garza García", "states_id" =>1]);
+       $city =  City::create(["name" => "Abasolo", "state_id" =>1]);
+       $city2 = City::create(["name" => "Bustamante", "state_id" =>1]);
+       $city3 = City::create(["name" => "Cadereyta Jiménez", "state_id" =>1]);
+       $city4 = City::create(["name" => "Doctor González", "state_id" =>1]);
+       $city5 = City::create(["name" => "San Pedro Garza García", "state_id" =>1]);
 
        //states->Chiapas
-       $city6 = City::create(["name" => "Acala", "states_id" =>2]);
-       $city7 = City::create(["name" => "Berriozábal", "states_id" =>2]);
-       $city8 = City::create(["name" => "Cacahoatán", "states_id" =>2]);
-       $city9 = City::create(["name" => "San Fernando", "states_id" =>2]);
-       $city10 = City::create(["name" => "Tuxtla Gutiérrez", "states_id" =>2]);
+       $city6 = City::create(["name" => "Acala", "state_id" =>2]);
+       $city7 = City::create(["name" => "Berriozábal", "state_id" =>2]);
+       $city8 = City::create(["name" => "Cacahoatán", "state_id" =>2]);
+       $city9 = City::create(["name" => "San Fernando", "state_id" =>2]);
+       $city10 = City::create(["name" => "Tuxtla Gutiérrez", "state_id" =>2]);
 
        //states->San Luis Potosí
-       $city11 = City::create(["name" => "Ahualulco", "states_id" =>3]);
-       $city12 = City::create(["name" => "Cárdenas", "states_id" =>3]);
-       $city13 = City::create(["name" => "Ebano", "states_id" =>3]);
-       $city14 = City::create(["name" => "Guadalcázar", "states_id" =>3]);
-       $city15 = City::create(["name" => "Moctezuma", "states_id" =>3]);
+       $city11 = City::create(["name" => "Ahualulco", "state_id" =>3]);
+       $city12 = City::create(["name" => "Cárdenas", "state_id" =>3]);
+       $city13 = City::create(["name" => "Ebano", "state_id" =>3]);
+       $city14 = City::create(["name" => "Guadalcázar", "state_id" =>3]);
+       $city15 = City::create(["name" => "Moctezuma", "state_id" =>3]);
 
        //states->Sonora
-       $city16 =  City::create(["name" => "Aconchi", "states_id" =>4]);
-       $city17 = City::create(["name" => "Bacadéhuachi", "states_id" =>4]);
-       $city18 = City::create(["name" => "Caborca", "states_id" =>4]);
-       $city19 = City::create(["name" => "Empalme", "states_id" =>4]);
-       $city20 = City::create(["name" => "San Felipe de Jesús", "states_id" =>4]);
+       $city16 =  City::create(["name" => "Aconchi", "state_id" =>4]);
+       $city17 = City::create(["name" => "Bacadéhuachi", "state_id" =>4]);
+       $city18 = City::create(["name" => "Caborca", "state_id" =>4]);
+       $city19 = City::create(["name" => "Empalme", "state_id" =>4]);
+       $city20 = City::create(["name" => "San Felipe de Jesús", "state_id" =>4]);
 
        //states->Morelos
-       $city21 =  City::create(["name" => "Axochiapan", "states_id" =>5]);
-       $city22 = City::create(["name" => "Coatlán del Río", "states_id" =>5]);
-       $city23 = City::create(["name" => "Emiliano Zapata", "states_id" =>5]);
-       $city24 = City::create(["name" => "Huitzilac", "states_id" =>5]);
-       $city25 = City::create(["name" => "Huitzilac", "states_id" =>5]);
+       $city21 =  City::create(["name" => "Axochiapan", "state_id" =>5]);
+       $city22 = City::create(["name" => "Coatlán del Río", "state_id" =>5]);
+       $city23 = City::create(["name" => "Emiliano Zapata", "state_id" =>5]);
+       $city24 = City::create(["name" => "Huitzilac", "state_id" =>5]);
+       $city25 = City::create(["name" => "Huitzilac", "state_id" =>5]);
     }
 }
--- database/seeders/StateSeeder.php
+++ database/seeders/StateSeeder.php
@@ -16,10 +16,10 @@ class StateSeeder extends Seeder
     public function run()
     {
         //contry -> mexico
-        $states =  State::create(["name" => "Nuevo León","countries_id" => 1]);
-        $states2 = State::create(["name" => "Chiapas","countries_id" => 1]);
-        $states3 = State::create(["name" => "San Luis Potosí","countries_id" => 1]);
-        $states4 = State::create(["name" => "Sonora","countries_id" => 1]);
-        $states5 = State::create(["name" => "Morelos","countries_id" => 1]);
+        $states =  State::create(["name" => "Nuevo León","country_id" => 1]);
+        $states2 = State::create(["name" => "Chiapas","country_id" => 1]);
+        $states3 = State::create(["name" => "San Luis Potosí","country_id" => 1]);
+        $states4 = State::create(["name" => "Sonora","country_id" => 1]);
+        $states5 = State::create(["name" => "Morelos","country_id" => 1]);
     }
 }
