ebd42101afb35d644778b316f82446f6c52fa2ac
Carlos Toledo <73927158+Carlosafk@users.noreply.github.com>
2022-11-10 11:39:20 -0600

    mas ocupaciones

--- database/seeders/OccupationSeeder.php
+++ database/seeders/OccupationSeeder.php
@@ -8,15 +8,100 @@ use Illuminate\Support\Facades\DB;
 
 class OccupationSeeder extends Seeder
 {
-    
+
     public function run()
     {
         DB::table('occupations')->insert([
-            ['name'=>'Bombero'],
-            ['name'=>'Dentista'],
-            ['name'=>'Enfermera'],
-            ['name'=>'Arquitecto'],
-            ['name'=>'Maestro']
+            [
+                'name' => 'Abogado'
+            ],
+            [
+                'name' => 'Ingeniero'
+            ],
+            [
+                'name' => 'Biólogo'
+            ],
+            [
+                'name' => 'Contador'
+            ],
+            [
+                'name' => 'Arquitecto'
+            ],
+            [
+                'name' => 'Periodista'
+            ],
+            [
+                'name' => 'Psicologo'
+            ],
+            [
+                'name' => 'Botánico'
+            ],
+            [
+                'name' => 'Farmacólogo'
+            ],
+            [
+                'name' => 'Cajero'
+            ],
+            [
+                'name' => 'Policía'
+            ],
+            [
+                'name' => 'Panadero'
+            ],
+            [
+                'name' => 'Escritor'
+            ],
+            [
+                'name' => 'Locutor'
+            ],
+            [
+                'name' => 'Carnicero',
+            ],
+            [
+                'name' => 'Albañil'
+            ],
+            [
+                'name' => 'Editor'
+            ],
+            [
+                'name' => 'Cocinero'
+            ],
+            [
+                'name' => 'Agricultor'
+            ],
+            [
+                'name' => 'Soldador'
+            ],
+            [
+                'name' => 'Sastre'
+            ],
+            [
+                'name' => 'Plomero'
+            ],
+            [
+                'name' => 'Pescador'
+            ],
+            [
+                'name' => 'Mecanico'
+            ],
+            [
+                'name' => 'Músico'
+            ],
+            [
+                'name' => 'Traductor'
+            ],
+            [
+                'name' => 'Carpintero'
+            ],
+            [
+                'name' => 'Cerrajero'
+            ],
+            [
+                'name' => 'Sastre'
+            ],
+            [
+                'name' => 'Minero'
+            ]
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -28,7 +28,7 @@ class UserSeeder extends Seeder
         $user->gender = "Masculino";
         $user->birthday = "1990-02-10";
         $user->save();
-
+//medicos
         $user = new User();
         $user->name = "Medico";
         $user->email = "medico@gmail.com";
@@ -74,11 +74,149 @@ class UserSeeder extends Seeder
         $user->birthday = "1990-02-10";
         $user->save();
 
+        $user = new User();
+        $user->name = "Marco";
+        $user->email = "medicomarco@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Masculino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Angel";
+        $user->email = "medicoangel@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Masculino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Miguel";
+        $user->email = "medicomiguel@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Masculino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Joel";
+        $user->email = "medicojoel@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Masculino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Alejandro";
+        $user->email = "medicoalejandro@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Masculino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
+        //pacientes
+        $user = new User();
+        $user->name = "Juan Jose";
+        $user->email = "juan_j@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Masculino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Jose I.";
+        $user->email = "josei@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Masculino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Marta";
+        $user->email = "marta@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Femenino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Luz M.";
+        $user->email = "luz_m@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Femenino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Josue";
+        $user->email = "josue@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Masculino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Maria";
+        $user->email = "maria@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Femenino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Fernanda";
+        $user->email = "fernanda@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Femenino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Maximiliano";
+        $user->email = "max@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Masculino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Daniela";
+        $user->email = "daniela@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Femenino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Marco Antonio";
+        $user->email = "marco_a@gmail.com";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";// password
+        $user->last_name = "prueba";
+        $user->gender = "Masculino";
+        $user->birthday = "1990-02-10";
+        $user->save();
+
 
         // ASIGNANDO ROLES A USUARIOS
         $user1 = User::where('id', 1)->first();
         $user1->assignRole(['Administrator']);
+//medicos
 
+        //pacientes
         $user2 = User::where('id', 2)->first();
         $user2->assignRole(['User', 'NewPatient']);
     }
