01be76b9c73a7119cb8fa2dc4488c219f0a23e0f
Carlos Toledo <73927158+Carlosafk@users.noreply.github.com>
2022-11-14 17:07:56 -0600

    correccion de migrates

--- database/migrations/2022_10_20_201405_create_patients_table.php
+++ database/migrations/2022_10_20_201405_create_patients_table.php
@@ -9,22 +9,17 @@ return new class () extends Migration {
     public function up()
     {
         Schema::create('patients', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('user_id')->unique();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->json('address');
             $table->string('zip_code');
             $table->string('country_code')->nullable();
             $table->string('emergency_number')->nullable();
             $table->json('id_card')->nullable();
-            $table->unsignedBigInteger('city_id')->nullable();
-            $table->foreign('city_id')->references('id')->on('cities')->onDelete('set null');
-            $table->unsignedBigInteger('health_insurance_id')->nullable();
-            $table->foreign('health_insurance_id')->references('id')->on('health_insurances')->onDelete('set null');
-            $table->unsignedBigInteger('medical_history_id')->nullable();
-            $table->foreign('medical_history_id')->references('id')->on('medical_histories')->onDelete('set null');
-            $table->unsignedBigInteger('medical_record_id')->nullable();
-            $table->foreign('medical_record_id')->references('id')->on('medical_records')->onDelete('set null');
+            $table->foreignId('city_id')->references('id')->on('cities');
+            $table->foreignId('health_insurance_id')->nullable()->constrained('health_insurances');
+            $table->foreignId('medical_history_id')->nullable()->constrained('medical_histories'); 
+            $table->foreignId('medical_record_id')->nullable()->constrained('medical_records');
             $table->timestamps();
             $table->softDeletes();
         });
