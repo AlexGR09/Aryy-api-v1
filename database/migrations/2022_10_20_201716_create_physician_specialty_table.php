db7f6b9301a66b32de733ee82e92e3dd3c402afd
Carlos Toledo <73927158+Carlosafk@users.noreply.github.com>
2022-11-15 10:23:53 -0600

    migrate

--- database/migrations/2022_10_20_201716_create_physician_specialty_table.php
+++ database/migrations/2022_10_20_201716_create_physician_specialty_table.php
@@ -10,7 +10,7 @@ return new class () extends Migration {
     {
         Schema::create('physician_specialty', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('specialty_id')->onDelete('set null')->constrained('specialties');
+            $table->foreignId('specialty_id')->constrained('specialties');
             $table->foreignId('physician_id')->nullable()->constrained('physicians');
             $table->string('license')->unique();
             $table->string('institution');
