0f70b48bad3bf676eab2881623b8ebbbabedd81b
Carlos Toledo <73927158+Carlosafk@users.noreply.github.com>
2022-12-14 18:04:48 -0600

    tabla de medicamento

--- database/migrations/2022_10_20_200205_create_medicines_table.php
+++ database/migrations/2022_10_20_200205_create_medicines_table.php
@@ -15,10 +15,13 @@ return new class() extends Migration
     {
         Schema::create('medicines', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('formula');
-            $table->string('Presentation');
-            $table->string('brand');
+            $table->string('tradename')->nullable();
+            $table->string('active')->nullable();
+            $table->string('presentation')->nullable();
+            $table->string('description')->nullable();
+            $table->string('contents')->nullable();
+            $table->string('laboratory')->nullable();
+            $table->string('price')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
