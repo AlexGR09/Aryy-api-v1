36e3f8980640425249248723441c4266292bfe51
Carlos Toledo <73927158+Carlosafk@users.noreply.github.com>
2022-12-26 15:22:51 -0600

    campo full_name calendar apointments

--- app/Http/Resources/API/V1/Physician/CalendarResource.php
+++ app/Http/Resources/API/V1/Physician/CalendarResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\API\V1\Physician;
 
+use App\Http\Resources\API\V1\Patient\PatientResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CalendarResource extends JsonResource
@@ -16,6 +17,7 @@ class CalendarResource extends JsonResource
     {
         return[
             'id_appointment' => $this->id,
+            'patient_full_name' => $this->patient->full_name,
             'appointment_date' => $this->appointment_date,
             'appointment_time' => $this->appointment_time,
             'appointment_type' => $this->appointment_type,
