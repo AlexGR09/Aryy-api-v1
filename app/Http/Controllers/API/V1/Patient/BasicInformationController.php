b603a06f78569f6cf115b2b044c0664a2e78579c
jtirado1996 <jorge.tirado@conexionbox.com>
2023-01-13 10:42:49 -0600

    vital signs

--- app/Http/Controllers/API/V1/Patient/BasicInformationController.php
+++ app/Http/Controllers/API/V1/Patient/BasicInformationController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\API\V1\Patient;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\Patient\StoreVitalSignRequest;
+use App\Http\Requests\API\Patient\UpdateVitalSignRequest;
 use App\Models\User;
+use App\Models\VitalSign;
 use Illuminate\Http\Request;
 
 class BasicInformationController extends Controller
@@ -14,10 +17,26 @@ class BasicInformationController extends Controller
         $medicalHistory = $patient->medicalHistory;
         $patientInfo = $patient->patient;
 
-        return ok('',[
-            'vital_sign' => $vitalSign, 
+        return ok('', [
+            'vital_sign' => $vitalSign,
             'medical_history' =>  $medicalHistory,
             'patient' => $patientInfo
         ]);
+    }
+
+    public function store(StoreVitalSignRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::find($data['user_id']);
+        $vitalSignCreated = $user->vitalSign->create($data);
+        return ok('', $vitalSignCreated);
+    }
+
+    public function update(UpdateVitalSignRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::find($data['user_id']);
+        $user->vitalSign->update($data);
+        return ok('',  $user->vitalSign);
     }
 }
