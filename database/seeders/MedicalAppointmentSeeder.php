bdcee7afb628db68a55ad83d6601c706eea8690c
Carlos Toledo <73927158+Carlosafk@users.noreply.github.com>
2023-01-17 12:06:47 -0600

    modificacion de seeder medical appointment

--- database/seeders/MedicalAppointmentSeeder.php
+++ database/seeders/MedicalAppointmentSeeder.php
@@ -13,7 +13,7 @@ class MedicalAppointmentSeeder extends Seeder
     {
         DB::table('medical_appointments')->insert([
             [
-                'appointment_date' => '2022-12-22',
+                'appointment_date' => '2023-01-17',
                 'appointment_time' => '5:00:00',
                 'appointment_time_end' => '5:30:00',
                 'appointment_type' => 'Consulta de primera vez',
@@ -23,7 +23,7 @@ class MedicalAppointmentSeeder extends Seeder
                 'prescription_id' => 1,
             ],
             [
-                'appointment_date' => '2020-12-22',
+                'appointment_date' => '2023-01-17',
                 'appointment_time' => '5:00:00',
                 'appointment_time_end' => '5:30:00',
                 'appointment_type' => 'Consulta de segunda vez',
@@ -33,7 +33,7 @@ class MedicalAppointmentSeeder extends Seeder
                 'prescription_id' => 2,
             ],
             [
-                'appointment_date' => '2023-01-22',
+                'appointment_date' => '2023-01-17',
                 'appointment_time' => '5:00:00',
                 'appointment_time_end' => '5:30:00',
                 'appointment_type' => 'Consulta de primera vez',
@@ -43,7 +43,7 @@ class MedicalAppointmentSeeder extends Seeder
                 'prescription_id' => 3,
             ],
             [
-                'appointment_date' => '2022-10-22',
+                'appointment_date' => '2023-01-17',
                 'appointment_time' => '5:00:00',
                 'appointment_time_end' => '5:30:00',
                 'appointment_type' => 'Consulta de primera vez',
@@ -53,7 +53,7 @@ class MedicalAppointmentSeeder extends Seeder
                 'prescription_id' => 4,
             ],
             [
-                'appointment_date'=>'2022-12-02',
+                'appointment_date'=>'2023-01-17',
                 'appointment_time'=>'5:00:00',
                 'appointment_time_end'=>'5:30:00',
                 'appointment_type'=>'Consulta de primera vez',
@@ -63,7 +63,7 @@ class MedicalAppointmentSeeder extends Seeder
                 'prescription_id' => 5,
             ],
             [
-                'appointment_date'=>'2022-10-05',
+                'appointment_date'=>'2023-01-17',
                 'appointment_time'=>'5:00:00',
                 'appointment_time_end'=>'5:30:00',
                 'appointment_type'=>'Consulta de primera vez',
@@ -73,7 +73,7 @@ class MedicalAppointmentSeeder extends Seeder
                 'prescription_id' => 6,
             ],
             [
-                'appointment_date'=>'2022-07-02',
+                'appointment_date'=>'2023-01-17',
                 'appointment_time'=>'5:00:00',
                 'appointment_time_end'=>'5:30:00',
                 'appointment_type'=>'Consulta de primera vez',
@@ -83,7 +83,7 @@ class MedicalAppointmentSeeder extends Seeder
                 'prescription_id' => 7,
             ],
             [
-                'appointment_date'=>'2023-10-01',
+                'appointment_date'=>'2023-01-17',
                 'appointment_time'=>'5:00:00',
                 'appointment_time_end'=>'5:30:00',
                 'appointment_type'=>'Consulta de primera vez',
@@ -93,7 +93,7 @@ class MedicalAppointmentSeeder extends Seeder
                 'prescription_id' => 8,
             ],
             [
-                'appointment_date'=>'2023-10-22',
+                'appointment_date'=>'2023-01-17',
                 'appointment_time'=>'5:00:00',
                 'appointment_time_end'=>'5:30:00',
                 'appointment_type'=>'Consulta de primera vez',
@@ -103,14 +103,14 @@ class MedicalAppointmentSeeder extends Seeder
                 'prescription_id' => 9,
             ],
             [
-                'appointment_date'=>'2022-12-28',
+                'appointment_date'=>'2023-01-17',
                 'appointment_time'=>'5:00:00',
                 'appointment_time_end'=>'5:30:00',
                 'appointment_type'=>'Consulta test',
                 'physician_id'=>10,
                 'patient_id'=>10,
                 'facility_id' => 10,
-                'prescription_id' => null,
+                'prescription_id' => 10,
             ]
         ]);
     }
