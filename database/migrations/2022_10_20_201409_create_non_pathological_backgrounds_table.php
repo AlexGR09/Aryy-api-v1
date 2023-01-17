9514680eed666a8db209b19aba8981b08c5c8862
Carlos Toledo <73927158+Carlosafk@users.noreply.github.com>
2023-01-17 15:41:32 -0600

    correccion de migrate non pathological_backgounds ->cambio de tipo de dato para el campo diet

--- database/migrations/2022_10_20_201409_create_non_pathological_backgrounds_table.php
+++ database/migrations/2022_10_20_201409_create_non_pathological_backgrounds_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->json('smoking')->nullable();
             $table->json('alcoholim')->nullable();
             $table->string('other_substances')->nullable();
-            $table->enum('diet', ['Dieta mediterránea', 'Dieta de la zona', 'Dieta vegetariana', 'Dieta vegana','Dieta de la fertilidad','Dieta hipocalórica','Dieta hipercalórica','Dieta volumétrica','Dieta keto','Dieta detox','Dieta Ornish / Ovolactovegetariana','Dieta Dash','Dieta paleo'])->nullable();
+            $table->enum('diet', ['Dieta mediterránea', 'Dieta de la zona', 'Dieta vegetariana', 'Dieta vegana', 'Dieta de la fertilidad', 'Dieta hipocalórica', 'Dieta hipercalórica', 'Dieta volumétrica', 'Dieta keto', 'Dieta detox', 'Dieta Ornish / Ovolactovegetariana', 'Dieta Dash', 'Dieta paleo'])->nullable();
             $table->json('current_medication')->nullable();
             $table->string('previous_medication')->nullable();
             $table->string('drug_active')->nullable();
