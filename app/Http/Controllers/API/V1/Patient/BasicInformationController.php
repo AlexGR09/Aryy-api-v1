2d796447c6d1875f620083beb0ec79654726ab5c
jtirado1996 <jorge.tirado@conexionbox.com>
2023-01-24 16:25:14 -0600

    changes vital signs

--- app/Http/Controllers/API/V1/Patient/BasicInformationController.php
+++ app/Http/Controllers/API/V1/Patient/BasicInformationController.php
@@ -6,16 +6,18 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\API\V1\Patient\StoreVitalSignRequest as PatientStoreVitalSignRequest;
 use App\Http\Requests\API\V1\Patient\UpdateVitalSignRequest as PatientUpdateVitalSignRequest;
 use App\Models\MedicalAppointment;
+use App\Models\MedicalHistory;
 use App\Models\Patient;
 use App\Models\Prescription;
 use App\Models\VitalSign;
 
 class BasicInformationController extends Controller
 {
-    public function show(Patient $patient)
+    public function show(Patient $patient, MedicalAppointment $medicalAppointment)
     {
         $patientInfo = clone $patient;
-        $vitalSign = $patient->vitalSign;
+        $vitalSignId = $medicalAppointment->prescription->vital_sign_id;
+        $vitalSign = VitalSign::find($vitalSignId);
         $medicalHistory = $patient->medicalHistory;
 
         return ok('', [
@@ -29,28 +31,32 @@ class BasicInformationController extends Controller
     {
         $data = $request->validated();
         $patient = Patient::find($data['patient_id']);
+        $medicalAppointment = MedicalAppointment::where(
+            [
+                ['id', $data['medical_appointment_id']],
+                ['patient_id', $patient->id]
+            ]
+        )
+        ->first();
+        if(empty($medicalAppointment)){
+            return not_found('Recurso no encontrado',[]);
+        }
+        if(!empty($medicalAppointment->prescription->vital_sign_id)){
+            return conflict('Este registro ya tiene una relacion signos vitales',[]);
+        }
         $vitalSign = VitalSign::create($data);
+        $medicalAppointment->prescription()->update(['vital_sign_id' =>  $vitalSign->id]);
 
-        $prescription = Prescription::create(['vital_sign_id' => $vitalSign->id]);
-        
-        MedicalAppointment::where('patient_id', $patient->id)
-        ->update(['prescription_id' => $prescription->id]);
-        
         return ok('', $vitalSign);
     }
 
-    public function update(Patient $patient, PatientUpdateVitalSignRequest $request)
+    public function update(Patient $patient, MedicalAppointment $medicalAppointment, PatientUpdateVitalSignRequest $request)
     {
         $data = $request->validated();
-        $patient = Patient::find($data['patient_id']);
-        $vitalSignCreated = $patient
-            ->medical_appointments()
-            ->prescription()
-            ->vitalSign()
-            ->update($patient);
-        return ok('', $patient
-            ->medical_appointments()
-            ->prescription()
-            ->vitalSign);
+        $vitalSignId = $medicalAppointment->where('patient_id', $patient->id)->first()->prescription->vital_sign_id;
+        $vitalSign = tap(VitalSign::find($vitalSignId))
+            ->update($data);
+
+        return ok('', $vitalSign);
     }
 }
