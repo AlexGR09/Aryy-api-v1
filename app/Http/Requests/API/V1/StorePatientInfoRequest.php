3da0c2ed4a652f9a96d3adf336f4587669c6e413
jtirado1996 <jorge.tirado@conexionbox.com>
2023-02-01 14:33:00 -0600

    Feature: New basic information route

---
+++ app/Http/Requests/API/V1/StorePatientInfoRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\API\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePatientInfoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'patient_id' => 'required',
+            'phone_number' => 'required',
+            'full_name' => 'required',
+            'age' => 'required',
+            'blood_type' => 'required',
+            'height' => 'required',
+            'gender' => 'required|in:Masculino,Femenino'
+        ];
+    }
+}
