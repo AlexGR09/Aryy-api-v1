b34061264f898f08f9ffa4410e27d435b0def97e
Carlos Toledo <73927158+Carlosafk@users.noreply.github.com>
2023-02-09 11:55:56 -0600

    correccion de favoritos

---
+++ app/Http/Resources/API/V1/Patient/Favorite2Resource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\API\V1\Patient;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Favorite2Resource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'physician_id' => $this->physician_id,
+            'physician' => (new FavoritePhysicianResource($this->physician)),
+            'is_favorite'=>boolval($this->is_favorite)
+        ];
+    }
+}
