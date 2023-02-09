a17a244acb057f6b8e5c55d0fce8bf200caf7638
jtirado1996 <jorge.tirado@conexionbox.com>
2023-02-09 15:42:36 -0600

    Fix: Facility Schedule request data and namespace

--- app/Http/Controllers/API/V1/Physician/FacilityScheduleController.php
+++ app/Http/Controllers/API/V1/Physician/FacilityScheduleController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\API\V1\Physician;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\FacilityScheduleRequest;
+use App\Http\Requests\API\V1\Physician\FacilityScheduleRequest as PhysicianFacilityScheduleRequest;
 use App\Http\Resources\API\V1\Physician\FacilityResource;
 use App\Models\Facility;
 
 class FacilityScheduleController extends Controller
 {
-    public function schedule(Facility $facility, FacilityScheduleRequest $request)
+    public function schedule(Facility $facility, PhysicianFacilityScheduleRequest $request)
     {
         return new FacilityResource(tap($facility)->update($request->validated()));
     }
