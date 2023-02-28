45ee5eb331936bbbc3304988dfa94d9c4c3521de
jtirado1996 <jorge.tirado@conexionbox.com>
2023-02-28 15:53:15 -0600

    Add more catalogues

--- database/seeders/CatalogueInsuranceSeeder.php
+++ database/seeders/CatalogueInsuranceSeeder.php
@@ -10,11 +10,33 @@ class CatalogueInsuranceSeeder extends Seeder
     public function run()
     {
         DB::table('insurances')->insert([
-            ['name' => 'Medicaid'],
-            ['name' => 'Mapfre'],
-            ['name' => 'Seguros banorte'],
-            ['name' => 'Metlife'],
-            ['name' => 'Allianz Seguros'],
+            ['name' =>  'AIG Seguros México'],
+            ['name' =>  'Allianz México'],
+            ['name' =>  'AXA Seguros'],
+            ['name' =>  'Chubb Seguros México'],
+            ['name' =>  'General de Seguros'],
+            ['name' =>  'Grupo Mexicano de Seguros'],
+            ['name' =>  'Grupo Nacional Provincial (GNP)'],
+            ['name' =>  'HDI Seguros'],
+            ['name' =>  'Inbursa Seguros'],
+            ['name' =>  'Mapfre México'],
+            ['name' =>  'MetLife México'],
+            ['name' =>  'Quálitas'],
+            ['name' =>  'RSA Seguros México'],
+            ['name' =>  'Seguros Atlas'],
+            ['name' =>  'Seguros Banca Afirme'],
+            ['name' =>  'Seguros Banca Mifel'],
+            ['name' =>  'Seguros BanCoppel'],
+            ['name' =>  'Seguros Banjercito'],
+            ['name' =>  'Seguros BBVA'],
+            ['name' =>  'Seguros GNP Fuerza Vital'],
+            ['name' =>  'Seguros HSBC'],
+            ['name' =>  'Seguros Inbursa'],
+            ['name' =>  'Seguros Interacciones'],
+            ['name' =>  'Seguros Liverpool'],
+            ['name' =>  'Seguros Monterrey'],
+            ['name' =>  'Seguros Santander'],
+            ['name' =>  'Zurich Seguros'],
         ]);
     }
 }
