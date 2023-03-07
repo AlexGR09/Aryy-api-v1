d4ae4c032ae5c271743a02c60539b6ab59486f3d
Carlos Toledo <73927158+Carlosafk@users.noreply.github.com>
2023-03-07 14:59:37 -0600

    medicamentos

--- database/seeders/NonPathologicalBackgroundSeeder.php
+++ database/seeders/NonPathologicalBackgroundSeeder.php
@@ -14,55 +14,61 @@ class NonPathologicalBackgroundSeeder extends Seeder
      */
     public function run()
     {
+        $medicine_ac = [
+            'Ibuprofeno • 10mg • Comprimido,Paracetamol - 10mg - Comprimido',
+            'Amoxilina • 10mg • Comprimido,Paracetamol - 10mg - Comprimido',
+            'Omeprazol • 10mg • Comprimido,Paracetamol - 10mg - Comprimido'
+        ];
+
         DB::table('non_pathological_backgrounds')->insert([
             [
                 'id' => 1,
-                'drug_active' => 'Ibuprofeno • 10mg • Comprimido,Paracetamol - 10mg - Comprimido',
+                'drug_active' =>json_encode($medicine_ac),
                 'previous_medication' => 'N/A',
             ],
             [
                 'id' => 2,
-                'drug_active' => 'Ibuprofeno • 10mg • Comprimido',
+                'drug_active' => json_encode($medicine_ac),
                 'previous_medication' => 'Paracetamol • 10mg • Comprimido',
             ],
             [
                 'id' => 3,
-                'drug_active' => 'Ibuprofeno • 10mg • Comprimido,Paracetamol • 10mg • Comprimido,Amoxilina • 10mg • Comprimido',
+                'drug_active' => json_encode($medicine_ac),
                 'previous_medication' => 'N/A',
             ],
             [
                 'id' => 4,
-                'drug_active' => 'Ibuprofeno • 10mg • Comprimido',
+                'drug_active' => json_encode($medicine_ac),
                 'previous_medication' => 'Naproxeno • 10mg • Comprimido',
             ],
             [
                 'id' => 5,
-                'drug_active' => 'Ibuprofeno • 10mg • Comprimido,Naproxeno • 10mg • Comprimido',
+                'drug_active' => json_encode($medicine_ac),
                 'previous_medication' => 'N/A',
             ],
             [
                 'id' => 6,
-                'drug_active' => 'Simvastatina • 10mg • Comprimido',
+                'drug_active' => json_encode($medicine_ac),
                 'previous_medication' => 'Ibuprofeno • 10mg • Comprimido',
             ],
             [
                 'id' => 7,
-                'drug_active' => 'Omeprazol • 10mg • Comprimido',
+                'drug_active' => json_encode($medicine_ac),
                 'previous_medication' => 'Aspirina • 10mg • Comprimido',
             ],
             [
                 'id' => 8,
-                'drug_active' => 'Ibuprofeno • 10mg • Comprimido,Aspirina • 10mg • Comprimido',
+                'drug_active' => json_encode($medicine_ac),
                 'previous_medication' => 'N/A',
             ],
             [
                 'id' => 9,
-                'drug_active' => 'Amlodipina • 10mg • Comprimido',
+                'drug_active' => json_encode($medicine_ac),
                 'previous_medication' => 'Ibuprofeno • 10mg • Comprimido',
             ],
             [
                 'id' => 10,
-                'drug_active' => 'Ibuprofeno • 10mg • Comprimido',
+                'drug_active' => json_encode($medicine_ac),
                 'previous_medication' => 'Salbutamol • 10mg • Comprimido',
             ],
 
