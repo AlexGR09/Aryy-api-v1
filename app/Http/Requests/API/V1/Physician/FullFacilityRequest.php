dbba217472635a6e694e3a0efb9eb5bcbfb4e7e7
Carlos Toledo <73927158+Carlosafk@users.noreply.github.com>
2023-03-07 10:48:38 -0600

    a

--- app/Http/Requests/API/V1/Physician/FullFacilityRequest.php
+++ app/Http/Requests/API/V1/Physician/FullFacilityRequest.php
@@ -42,16 +42,16 @@ class FullFacilityRequest extends FormRequest
             'accessibility_and_others.accessibility.wheelchair_ramp' => 'required',
             'accessibility_and_others.accessibility.wheelchair_lift' => 'required',
 
-            'accessibility_and_others.accessibility.toilets_with_wheelchair_access' => 'required',
-            'accessibility_and_others.accessibility.rest_area_with_wheelchair_access' => 'required',
-            'accessibility_and_others.accessibility.staff_trained_in_sign_language' => 'required',
-            'accessibility_and_others.accessibility.braille_signage_for_blind_people' => 'required',
-            'accessibility_and_others.usual_audiences.lgtb_friendly' => 'required',
-            'accessibility_and_others.usual_audiences.safe_space_for_transgender_people' => 'required',
-            'accessibility_and_others.services.toilets' => 'required',
-            'accessibility_and_others.services.unisex_toilets' => 'required',
-            'accessibility_and_others.services.wifi' => 'required',
-            'coordinates' => 'required',
+            'accessibility_and_others.accessibility.toilets_with_wheelchair_access' => 'nullable',
+            'accessibility_and_others.accessibility.rest_area_with_wheelchair_access' => 'nullable',
+            'accessibility_and_others.accessibility.staff_trained_in_sign_language' => 'nullable',
+            'accessibility_and_others.accessibility.braille_signage_for_blind_people' => 'nullable',
+            'accessibility_and_others.usual_audiences.lgtb_friendly' => 'nullable',
+            'accessibility_and_others.usual_audiences.safe_space_for_transgender_people' => 'nullable',
+            'accessibility_and_others.services.toilets' => 'nullable',
+            'accessibility_and_others.services.unisex_toilets' => 'nullable',
+            'accessibility_and_others.services.wifi' => 'nullable',
+            'coordinates' => 'nullable',
         ];
     }
 }
