4bbaa599cfdf7e24a9192970619b2a69edce3ec8
Carlos Toledo <73927158+Carlosafk@users.noreply.github.com>
2023-03-08 14:54:03 -0600

    correccion modelo physicianspecialty

--- app/Models/PhysicianSpecialty.php
+++ app/Models/PhysicianSpecialty.php
@@ -17,7 +17,7 @@ class PhysicianSpecialty extends Model
         'specialty_id',
         'physician_id',
         'license',
-        'lincense_photo',
+        'license_photo',
         'institution',
     ];
 
