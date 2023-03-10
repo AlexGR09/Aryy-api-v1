46c8a0c12f1cdb5e8c8fefac5b8d133df41aa896
Carlos Toledo <73927158+Carlosafk@users.noreply.github.com>
2023-03-10 12:07:58 -0600

    actualizacion de resource

--- app/Http/Resources/API/V1/Patient/PathologicalBackgroundResource.php
+++ app/Http/Resources/API/V1/Patient/PathologicalBackgroundResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\API\V1\Patient;
 
+use App\Models\PathologicalBackground;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class PathologicalBackgroundResource extends JsonResource
@@ -13,8 +14,24 @@ class PathologicalBackgroundResource extends JsonResource
      */
     public function toArray($request): array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
     {
+        $patotlogias = PathologicalBackground::where('id', $this->id)->first();
+
+        $array = json_decode($patotlogias, true);
+
+        // Obtener los elementos no nulos
+        $nonNullArray = array_filter($array, function ($value) {
+            return !is_null($value);
+        });
+
+        // Obtener los elementos nulos y dividirlos en trozos de 2
+        $nullArrayChunks = array_chunk(array_filter($array, function ($value) {
+            return is_null($value);
+        }), 2);
+
+        /* print_r($nonNullArray);
+        print_r($nullArrayChunks); */
         return [
-            'pathological_background_id' => $this->id,        
+            /* 'pathological_background_id' => $this->id,        
             'previous_surgeries' => $this->previous_surgeries,
             'blood_transfusions' => $this->blood_transfusions,
             'diabetes' => $this->diabetes,
@@ -28,7 +45,9 @@ class PathologicalBackgroundResource extends JsonResource
             'respiratory_diseases' => $this->respiratory_diseases,
             'ets' => $this->ets,
             'gastrointestinal_pathologies' => $this->gastrointestinal_pathologies,
-            'blood_diseases' => $this->blood_diseases,
+            'blood_diseases' => $this->blood_diseases, */
+            'campos llenos'=> $nonNullArray,
+            'campos nulos'=> $nullArrayChunks
         ];
     }
 }
