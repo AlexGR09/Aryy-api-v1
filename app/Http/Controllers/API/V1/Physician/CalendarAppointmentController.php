7452a74971775234ffbe8dab4366baa95aa85a18
Carlos Toledo <73927158+Carlosafk@users.noreply.github.com>
2023-03-13 14:45:29 -0600

    pruebas de fecha -store calendar appointments

--- app/Http/Controllers/API/V1/Physician/CalendarAppointmentController.php
+++ app/Http/Controllers/API/V1/Physician/CalendarAppointmentController.php
@@ -116,10 +116,12 @@ class CalendarAppointmentController extends Controller
             $appointmentDate = Carbon::createFromFormat('Y-m-d',$request->appointment_date);
             $appointmentDate=$appointmentDate->format('Y-m-d');
 
-            $appointmentTime = strtotime($request->appointment_time);
+            $appointmentTime = Carbon::createFromFormat('H:i:s', $request->appointment_time, 'UTC');
+
+            $appointmentTime = strtotime($appointmentTime);
             $dateTime = date('H:i:s',$appointmentTime);
 
-            $time = strtotime($request->appointment_time) + strtotime($facility->consultation_length); //SUMA LA DURACION DE LA CONSULTA A LA HORA DE LA CITA
+            $time = strtotime($dateTime) + strtotime($facility->consultation_length); //SUMA LA DURACION DE LA CONSULTA A LA HORA DE LA CITA
             $date_time_end = date('H:i:s', $time); //SE LE DA EL FORMATO DE HORA */
 
 
